c95c09109ce5cc63b8d67028b6faab7cca1e69c9
Ashish Kumar <Ashish.Kumar@go-mmt.com>
2020-11-27 23:39:25 +0530

    added new api

--- src/main/java/com/tindia/demo/controller/HelloController.java
+++ src/main/java/com/tindia/demo/controller/HelloController.java
@@ -1,11 +1,15 @@
 package com.tindia.demo.controller;
 
 
+import com.tindia.demo.model.Film;
 import com.tindia.demo.model.Greeting;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
 
 @RestController
@@ -23,7 +27,23 @@ public class HelloController {
 
     @GetMapping("/bundle")
     public String greeting1(@RequestParam(value = "name", defaultValue = "World") String name) {
-        System.out.println("Got a new connection ....");
         return "THis is bundlew";
+    }
+
+
+    @GetMapping("/films")
+    public List<Film> film(@RequestParam(value = "name", defaultValue = "World") String name) {
+        List list = new ArrayList<Film>();
+
+        Film f1 = Film.builder().id(1).description("des").title("sholay").director("sippy").build();
+        Film f2 = Film.builder().id(2).description("des").title("sholay1").director("sippy").build();
+        Film f3 = Film.builder().id(3).description("des").title("sholay2").director("sippy").build();
+        Film f4 = Film.builder().id(4).description("des").title("sholay3").director("sippy").build();
+
+        list.add(f1);
+        list.add(f2);
+        list.add(f3);
+        list.add(f4);
+        return list;
     }
 }
